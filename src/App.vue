4d2c54b5630f4d04147452c7da7fbe867f862b3a
huanganxuan <1039215157@qq.com>
2022-05-20 20:27:15 +0800

    更新首页样式

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
 <template>
-  <el-container :class="$style.container">
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-header>Hello World</el-header>
-      <el-main>
-        <router-view />
-      </el-main>
-    </el-container>
-  </el-container>
+  <div :class="$style.container">
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss" module>
 .container {
   height: 100%;
   width: 100%;
-  :global {
-    .el-aside {
-      background-color: #001529;
-      box-shadow: 2px 0 8px 0 rgb(29 35 41 / 5%);
-    }
-    .el-header {
-      height: 48px;
-      line-height: 48px;
-      background: #fff;
-      box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
-    }
-    .el-main {
-      background-color: #eff2f5;
-    }
-  }
+  overflow: hidden;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div :class="$style.container">
-    <el-skeleton :loading="loading" :rows="5" animated>
-      <el-empty>
-        <el-button type="primary">Button</el-button>
-      </el-empty>
-    </el-skeleton>
+    <h1 class="welcome">
+      <span class="first-line">I Am HuangAnXuan</span>
+      <span class="second-line">Welcome To My Website</span>
+    </h1>
   </div>
 </template>
 
@@ -30,8 +29,58 @@ export default defineComponent({
 .container {
   height: 100%;
   width: 100%;
-  background-color: #fff;
-  padding: 15px;
-  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &::after {
+    position: absolute;
+    left: -20px;
+    right: -20px;
+    top: -20px;
+    bottom: -20px;
+    background: url('~@/assets/background.jpeg') no-repeat 50%/cover;
+    -webkit-filter: blur(10px);
+    filter: blur(10px);
+    content: "";
+  }
+  & > * {
+    position: relative;
+    z-index: 1;
+  }
+}
+</style>
+<style lang="scss">
+@keyframes typing {
+  from { width: 0 }
+}
+
+@keyframes caret {
+  50% { border-right-color: currentColor; }
+}
+
+h1.welcome {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  line-height: 70px;
+  font-weight: 700;
+  font-size: 50px;
+  color: #fff;
+  .first-line, .second-line {
+    font: bold 100% Consolas, Monaco, monospace;
+    overflow: hidden;
+    border-right: 1px solid transparent;
+    font-family: Consolas,Monaco,Monospace;
+    white-space: nowrap;
+    text-shadow: 3px 3px 5px rgb(0 0 0 / 50%);
+  }
+  .first-line {
+    width: 16ch;
+    animation: typing 2s steps(16) 0s backwards,caret .5s steps(1) 0s 4 forwards;
+  }
+  .second-line {
+    width: 22ch;
+    animation: typing 2s steps(22) 2s backwards,caret .5s steps(1) 2s 4 forwards;
+  }
 }
 </style>
