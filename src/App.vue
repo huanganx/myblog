8f053b41021fd06545426574707fd74b13883e5e
huanganxuan <1039215157@qq.com>
2022-05-20 23:04:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  mounted () {
+    this.$router.replace('/welcome')
+  }
+})
+</script>
+
 <style lang="scss" module>
 .container {
   height: 100%;
